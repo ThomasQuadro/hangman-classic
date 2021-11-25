f0852d1f60fb702e1e95a52b4acb144e964abe30
Thomas Quadro <91244260+ThomasQuadro@users.noreply.github.com>
2021-11-25 19:17:47 +0100

    first

--- fait/randomword.go
+++ fait/randomword.go
@@ -12,6 +12,7 @@ import (
 var motjeu string
 var crypto []string
 
+//Fonction qui prend un mot aléatoirement dans la liste de mots .txt
 func mothasard() string {
 	file, err := ioutil.ReadFile("word/words.txt")
 	if err != nil {
@@ -25,14 +26,29 @@ func mothasard() string {
 	return motjeu
 }
 
-func crypt() []string {
+//Fonction qui dresse des lignes a la place des lettres
+func crypt() string {
+	var line string
 	for i := 0; i < len(motjeu); i++ {
 		crypto = append(crypto, "_")
+		line = line + crypto[i] + " "
 	}
-	return crypto
+	return line
 }
 
+//Fonction qui prend une lettre du mot sélectionné aléatoirement pour commencer le hangman
+func motvisiblefirst() {
+	var alea = rand.Intn(len(motjeu))
+	// fmt.Println(alea)
+	// fmt.Println(string(motjeu[alea]))
+	crypto[alea] = string(motjeu[alea])
+	fmt.Println(crypt())
+
+}
+
+//Fonction print
 func main() {
 	fmt.Println(mothasard())
 	fmt.Println(crypt())
+	motvisiblefirst()
 }
--- main.go
+++ main.go
@@ -3,20 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var i rune
-RETOUR:
+	var i string
+	// RETOUR:
 	fmt.Println("Entrez une Lettre")
-	fmt.Scan(&i)
-	// si c'est vrais
+	fmt.Scanf("%s", &i)
+	// fmt.Print(string(i))
 
+	switch i {
+	case "L":
+		fmt.Println("bonjour")
+		// si c'est vrais
+	}
 	// Sinon
 	//fmt.Println("Vous avez choisis la lettre", i, "Mauvais choix")
 
-	if i > rune(91) || i < rune(64) {
-		fmt.Println("Désolé, lette indisponnible")
-		goto RETOUR
-	} else {
-		fmt.Println("Vous avez choisis la lettre", string(i), "Bon choix")
-	}
+	// if i > rune(91) || i < rune(64) {
+	// 	fmt.Println("Désolé, lette indisponnible")
+	// 	goto RETOUR
+	// } else {
+	// 	fmt.Println("Vous avez choisis la lettre", string(i), "Bon choix")
+	// }
 
 }
