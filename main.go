39fe365a3a6e1680aee84688a3691c67e26ec7cd
Rom1 <romain.collesipomella@ynov.com>
2021-12-02 09:43:42 +0100

    interaction

--- main.go
+++ main.go
@@ -2,21 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
+var letter = "I"
+var l = []rune(letter)
+
+func verifyIsLetter() {
+
 	var i rune
 RETOUR:
 	fmt.Println("Entrez une Lettre")
 	fmt.Scan(&i)
-	// si c'est vrais
-
-	// Sinon
-	//fmt.Println("Vous avez choisis la lettre", i, "Mauvais choix")
-
-	if i > rune('Z') || i < rune('A') {
+	if i > rune('Z') || i < rune('A') || i > rune('z') || i < rune('a') {
 		fmt.Println("Désolé, lettre indisponnible")
 		goto RETOUR
-	} else if i < rune('Z') || i > rune('A') {
+
+	} else {
 		fmt.Println("Vous avez choisis la lettre", string(i), "Bon choix")
 	}
+}
 
+func main() {
+	fmt.Println(l)
+	// fmt.Println(verifyIsLetter())
 }
